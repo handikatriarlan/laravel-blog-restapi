e4364a07eafa0279e934210a127d8539f5281bb6
Arlann <handikaarlan@gmail.com>
2024-10-23 19:46:38 +0700

    feat: add error handling and exception

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Post;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {
