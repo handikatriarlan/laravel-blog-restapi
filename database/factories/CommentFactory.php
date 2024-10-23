3309ff02159962385e9035f49679293e755339cb
Arlann <handikaarlan@gmail.com>
2024-10-23 14:00:16 +0700

    feat: starter project

---
+++ database/factories/CommentFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Comment;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CommentFactory extends Factory
+{
+    protected $model = Comment::class;
+
+    public function definition()
+    {
+        return [
+            'user_id' => $this->faker->numberBetween(1, 10),
+            'post_id' => $this->faker->numberBetween(1, 20),
+            'body' => $this->faker->paragraph(3)
+        ];
+    }
+}
